1d5b70420cb42bb03f1903179c7e286c48ec93c7
我若为王 <807190135@qq.com>
2021-05-14 13:42:20 +0800

    Create one.sh

---
+++ ssh/one.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+key(){
+bash <(curl -fsSL git.io/key.sh) -og ilay1678 -p 8071 -d
+apt-get install ufw -y
+ufw allow 8071
+ufw allow https
+echo y | ufw enable
+}
+
+v2ray(){
+bash <(curl -s -L https://git.io/v2ray.sh)
+}
+
+nezha(){
+bash <(curl -s -L https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh)
+}
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+export PATH=$PATH:/usr/local/bin
+show_menu() {
+    echo -e "
+    ${green}我若为王${plain} ${red}${plain}
+    --- https://ifking.cn ---
+    ${green}1.${plain}  一键开启ssh秘钥登录
+    ${green}2.${plain}  v2ray脚本
+    ${green}3.${plain}  哪吒面板脚本
+    ————————————————-
+    ${green}0.${plain}  退出脚本
+    "
+    echo && read -p "请输入选择 [0-13]: " num
+
+    case "${num}" in
+    0)
+        exit 0
+        ;;
+    1)
+        key
+        ;;
+    2)
+        v2ray
+        ;;
+    3)
+        nezha
+        ;;
+    *)
+        echo -e "${red}请输入正确的数字 [0-3]${plain}"
+                show_menu
+        ;;
+    esac
+}
+show_menu
